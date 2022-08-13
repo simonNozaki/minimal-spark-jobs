1e3c5b3e072d2a8d96656a4ceb0357be0f37b63a
snozaki <snozaki@netprotections.co.jp>
2022-08-13 20:52:00 +0900

    mod: シャード数分だけDStreamを生成する

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Apache SparkのHello World。
 
 - `JsonEtlMain`: spark sql、ジョインしてcsvにする
 - `KinesisStreamingMain`: AWS Kinesis のストリームを受信して加工・json書き出し
+
+## お役立ち
+https://supergloo.com/spark-streaming/spark-kinesis-example/
--- src/main/scala/KinesisStreamingMain.scala
+++ src/main/scala/KinesisStreamingMain.scala
@@ -1,3 +1,5 @@
+import com.amazonaws.auth.{AWSStaticCredentialsProvider, DefaultAWSCredentialsProviderChain}
+import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
@@ -26,31 +28,49 @@ object KinesisStreamingMain {
     }
   }
 
+  private val streamName = "dev-todo-items-data-stream"
+
   def main(sysArgs: Array[String]): Unit = {
     val sparkConf = new SparkConf()
-      .setMaster("local[*]")
       .setAppName("kinesis-streaming-job")
+    sparkConf.setIfMissing("spark.master", "local[*]")
     // 1sec置きにストリームを監視
     val ssc = new StreamingContext(sparkConf, Seconds(1))
 
+    val credentials = new DefaultAWSCredentialsProviderChain().getCredentials
+    val kinesisClient = AmazonKinesisClientBuilder
+      .standard()
+      .withCredentials(new AWSStaticCredentialsProvider(credentials))
+      .build()
+    val shardCounts = kinesisClient
+      .describeStream(streamName)
+      .getStreamDescription
+      .getShards
+      .size
+
     // エンドポイントがデフォルトで "https://kinesis.us-east-1.amazonaws.com" になっている...
     // ルールに沿って定義必要: https://docs.aws.amazon.com/general/latest/gr/rande.html
-    val stream = KinesisInputDStream.builder
-      .streamingContext(ssc)
-      .endpointUrl("https://kinesis.ap-northeast-1.amazonaws.com")
-      .regionName("ap-northeast-1")
-      .streamName("dev-todo-items-data-stream")
-      .checkpointAppName("dev-todo-items-data-stream-app")
-      .build()
+    val kinesisStreams = (0 until shardCounts).map { i =>
+      KinesisInputDStream.builder
+        .streamingContext(ssc)
+        .endpointUrl("https://kinesis.ap-northeast-1.amazonaws.com")
+        .regionName("ap-northeast-1")
+        .streamName("dev-todo-items-data-stream")
+        .checkpointAppName("dev-todo-items-data-stream-app")
+        .build()
+    }
 
-    stream.flatMap(b => b.mkString("Array(", ", ", ")")).foreachRDD{rdd: RDD[Char] => {
-      println(s"${LocalDateTime.now.toString} : ストリーミング中...")
-      val sparkSession = SparkSessionSingleton.getInstance(rdd.sparkContext.getConf)
-      import sparkSession.implicits._
+    ssc
+      .union(kinesisStreams)
+      .flatMap(byteArray => new String(byteArray).split(","))
+      .foreachRDD{rdd: RDD[String] => {
+        println(s"${LocalDateTime.now.toString} : ストリーミング中...")
+        val sparkSession = SparkSessionSingleton.getInstance(rdd.sparkContext.getConf)
+        import sparkSession.implicits._
 
-      val data = new String(rdd.collect())
-      println(data)
-    }}
+        val df = rdd.toDF()
+        df.write.format("console")
+      }}
 
     ssc.start
     ssc.awaitTermination
